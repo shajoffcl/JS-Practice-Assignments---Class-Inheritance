40ac919e7127e38820460084791edc968455e6df
Mohd Shaib <shaj.native@gmail.com>
2020-11-09 15:36:49 +0530

    changes done

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,16 @@
 
 // declare class
 
-
 // export class using module.exports
+
+const Shape = require("./shape");
+
+class Circle extends Shape {
+  calculateArea() {
+    console.log("Circle area");
+  }
+}
+
+let c = new Circle();
+c.calculateArea();
+c.drawShape();
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
 // export class using module.exports
+
+class Shape {
+  constructor() {
+    this.color = "Blue";
+  }
+  drawShape() {
+    console.log("Draw shape method");
+  }
+  calculateArea() {
+    console.log("Shape area");
+  }
+}
+
+module.exports = Shape;
